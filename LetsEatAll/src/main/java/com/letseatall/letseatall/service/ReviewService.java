baa06b8b2d49b8d46fe9e0ce7aa276a540e933ec
ChAnGMiNBae <127672347+ChAnGMiNBae@users.noreply.github.com>
2023-06-01 12:49:36 +0900

    Update ReviewService.java
    리뷰 인터페이스 수정

--- LetsEatAll/src/main/java/com/letseatall/letseatall/service/ReviewService.java
+++ LetsEatAll/src/main/java/com/letseatall/letseatall/service/ReviewService.java
@@ -1,22 +1,13 @@
 package com.letseatall.letseatall.service;
 
-import com.letseatall.letseatall.data.Entity.Review;
 import com.letseatall.letseatall.data.dto.Review.ReviewDto;
-import com.letseatall.letseatall.data.dto.Review.ReviewModifyDto;
 import com.letseatall.letseatall.data.dto.Review.ReviewResponseDto;
-import com.letseatall.letseatall.data.repository.MenuRepository;
-import com.letseatall.letseatall.data.repository.ReviewRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 public interface ReviewService {
-    ReviewResponseDto saveReview(ReviewDto reviewDto);
-    ReviewResponseDto getReview(Long id);
-    ReviewResponseDto modifyReview(ReviewModifyDto rmd);
-    Long deleteReview(Long id);
-    List<ReviewResponseDto> getAllReviewsInMenu(Long mid);
-    List<ReviewResponseDto> getAllReviewsInRestaurant(Long rid);
-    List<ReviewResponseDto> getAllReviewsInFranchise(Long fid);
-    List<ReviewResponseDto> getReviewsForUser(Long uid);
+    ReviewResponseDto saveReview(ReviewDto reviewDto);      // 리뷰 작성
+    ReviewResponseDto getReview(Long id);                   // 리뷰 조회
+    ReviewResponseDto getReviewsForRestaurant(Long id);                   // 리뷰 조회(해당 레스토랑의 모든 리뷰 조회)
+    ReviewResponseDto getReviewsForUser(Long id);                   // 리뷰 조회(해당 사용자가 작성한 모든 리뷰 조회)
+    ReviewResponseDto updateReview(Long id, ReviewDto reviewDto); // 리뷰 수정
+    void deleteReview(Long id);                             // 리뷰 삭제
 }
