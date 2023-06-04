53d18f1e8770f9d8b158f38002caebdb6bb4f091
ChAnGMiNBae <127672347+ChAnGMiNBae@users.noreply.github.com>
2023-06-05 00:32:33 +0900

    Update ReviewRepository.java
    findAllByUid 추가

--- LetsEatAll/src/main/java/com/letseatall/letseatall/data/repository/ReviewRepository.java
+++ LetsEatAll/src/main/java/com/letseatall/letseatall/data/repository/ReviewRepository.java
@@ -1,25 +1,11 @@
 package com.letseatall.letseatall.data.repository;
 
 import com.letseatall.letseatall.data.Entity.Review;
-import com.letseatall.letseatall.data.dto.Review.ReviewModifyDto;
-import com.letseatall.letseatall.data.dto.Review.ReviewResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    @Query("select r from Review r where r.menu.id = ?1")
-    List<Review> findAllByMenu(Long mid);
-
-    @Query("select r from Review r where r.menu.restaurant.id = ?1")
-    List<Review> findAllByRestaurant(Long rid);
-    @Query("select r.id from Review r where r.menu.restaurant.id = ?1")
-    List<Long> findIdAllByRestaurant(Long rid);
-    @Query("select r from Review r where r.menu.franchise.id = ?1")
-    List<Review> findAllByFranchise(Long id);
-    @Query("select r.id from Review r where r.menu.franchise.id = ?1")
-    List<Long> findIdAllByFranchise(Long id);
-
-    List<Review> findAllByWriterId(Long id);
+    List<Review> findAllByMid(Long mid);
+    List<Review> findAllByUid(Long uid);
 }
