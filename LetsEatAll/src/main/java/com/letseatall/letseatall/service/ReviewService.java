ba3aac9d826073123ddeeea478fe6a7f66502a24
ChAnGMiNBae <127672347+ChAnGMiNBae@users.noreply.github.com>
2023-06-05 00:33:56 +0900

    Update ReviewService.java
    해당 레스토랑의 모든 리뷰 -> 해당 메뉴의 모든 리뷰로 수정

--- LetsEatAll/src/main/java/com/letseatall/letseatall/service/ReviewService.java
+++ LetsEatAll/src/main/java/com/letseatall/letseatall/service/ReviewService.java
@@ -3,11 +3,13 @@ package com.letseatall.letseatall.service;
 import com.letseatall.letseatall.data.dto.Review.ReviewDto;
 import com.letseatall.letseatall.data.dto.Review.ReviewResponseDto;
 
+import java.util.List;
+
 public interface ReviewService {
     ReviewResponseDto saveReview(ReviewDto reviewDto);      // 리뷰 작성
     ReviewResponseDto getReview(Long id);                   // 리뷰 조회
-    ReviewResponseDto getReviewsForRestaurant(Long id);                   // 리뷰 조회(해당 레스토랑의 모든 리뷰 조회)
-    ReviewResponseDto getReviewsForUser(Long id);                   // 리뷰 조회(해당 사용자가 작성한 모든 리뷰 조회)
+    List<ReviewResponseDto> getReviewsForMenu(Long id);                   // 리뷰 조회(해당 메뉴의 모든 리뷰 조회)
+    List<ReviewResponseDto> getReviewsForUser(Long id);                 // 리뷰 조회(해당 사용자가 작성한 모든 리뷰 조회)
     ReviewResponseDto updateReview(Long id, ReviewDto reviewDto); // 리뷰 수정
     void deleteReview(Long id);                             // 리뷰 삭제
 }
