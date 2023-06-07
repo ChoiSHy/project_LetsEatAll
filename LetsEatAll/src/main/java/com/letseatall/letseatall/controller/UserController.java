84ea20e6f94ccc91219d60e5c265afea485bc8fd
ChoiSHy <tjdgus4697@gmail.com>
2023-06-07 21:29:16 +0900

    aadfsadfasdf

--- LetsEatAll/src/main/java/com/letseatall/letseatall/controller/UserController.java
+++ LetsEatAll/src/main/java/com/letseatall/letseatall/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/User")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
