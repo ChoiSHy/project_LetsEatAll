8c3de1edaa0748c3103599b22628a13c288338ea
ChAnGMiNBae <127672347+ChAnGMiNBae@users.noreply.github.com>
2023-11-04 02:53:49 +0900

    Update YoutubeService.java

--- LetsEatAll/src/main/java/com/letseatall/letseatall/service/YoutubeService.java
+++ LetsEatAll/src/main/java/com/letseatall/letseatall/service/YoutubeService.java
@@ -1,4 +1,15 @@
 package com.letseatall.letseatall.service;
 
 public interface YoutubeService {
+  // YouTube 자막 정보를 저장
+    YoutubeDto saveYoutubeCaption(YoutubeDto youtubeDto);
+
+    // YouTube 자막 정보를 업데이트
+    YoutubeDto updateYoutubeCaption(Long id, YoutubeDto youtubeDto);
+
+    // YouTube 자막 정보를 삭제
+    void deleteYoutubeCaption(Long id);
+
+    // 특정 YouTube 자막 정보를 조회
+    YoutubeDto getYoutubeCaptionById(Long id);
 }
