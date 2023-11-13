0ee6b649f6f91a86574d7cc42710492ab7654c5f
ChoiSHy <tjdgus4697@gmail.com>
2023-11-13 18:39:59 +0900

    WORKING:  - aws test 중  - Review modify, delete  - 수정 중 이미지 저장/삭제 기능 DONE: TODO:  - Review: User's review List  - Review: image 동반해서 보내는 기능 추가

--- LetsEatAll/src/test/java/com/letseatall/letseatall/LetsEatAllApplicationTests.java
+++ LetsEatAll/src/test/java/com/letseatall/letseatall/LetsEatAllApplicationTests.java
@@ -1,13 +1,6 @@
-package com.letseatall.letseatall;
+package java.com.letseatall.letseatall;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class LetsEatAllApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
